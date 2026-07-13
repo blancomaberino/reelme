2bf6def4903df10c9ab0e39560f31ef40f0ae842
Marcelo Blanco <blancomaberino@gmail.com>
2026-07-13 18:43:14 -0300

    fix(api): import PlaceListFactory (Pint fully_qualified_strict_types)

--- apps/api/app/Models/PlaceList.php
+++ apps/api/app/Models/PlaceList.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Database\Factories\PlaceListFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -21,7 +22,7 @@ use Illuminate\Support\Str;
  */
 class PlaceList extends Model
 {
-    /** @use HasFactory<\Database\Factories\PlaceListFactory> */
+    /** @use HasFactory<PlaceListFactory> */
     use HasFactory;
 
     protected $fillable = ['name', 'is_public'];
